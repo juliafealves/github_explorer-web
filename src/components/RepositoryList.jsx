118dd4285e811a3a29951227c7fcbf1e065f6d0c
Júlia Fernandes Alves <juliafealves@gmail.com>
2021-12-06 23:43:22 -0300

    Added first component.

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,14 @@
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Repository List</h1>
+            <ul>
+                <li>
+                    <strong>uniform</strong>
+                    <p>Forms in React.</p>
+                    <a href="">Access repository</a>
+                </li>
+            </ul>
+        </section>
+    );
+}
